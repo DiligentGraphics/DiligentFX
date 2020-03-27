046d57afdd211bc9e22ac560edd37348965161b4
assiduous <assiduous@diligentgraphics.com>
2020-03-26 19:54:48 -0700

    Enabled format validation on Linux

--- BuildTools/FormatValidation/validate_format_linux.sh
+++ BuildTools/FormatValidation/validate_format_linux.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-echo "Format validation is not yet enabled for DiligentFX module on Linux"
-
+python ../../../DiligentCore/BuildTools/FormatValidation/clang-format-validate.py \
+--clang-format-executable ../../../DiligentCore/BuildTools/FormatValidation/clang-format_linux_10.0.0 \
+-r ../../Components ../../GLTF_PBR_Renderer ../../PostProcess ../../Utilities ../../Tests
