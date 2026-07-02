abe1f8915f97840b794a6cd7f98bc7800f5519c7
assiduous <assiduous@diligentgraphics.com>
2026-07-02 15:32:42 -0700

    Radient: move material asset tests to dedicated file
    Move the material creation test out of RadientSceneTest and into a
    RadientMaterialAssetManagerTest file. Create the material manager directly so
    the test focuses on material asset behavior instead of full scene setup.

---
+++ Tests/RadientTest/src/RadientMaterialAssetManagerTest.cpp
@@ -0,0 +1,78 @@
+/*
+ *  Copyright 2026 Diligent Graphics LLC
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  In no event and under no legal theory, whether in tort (including negligence),
+ *  contract, or otherwise, unless required by applicable law (such as deliberate
+ *  and grossly negligent acts) or agreed to in writing, shall any Contributor be
+ *  liable for any damages, including any direct, indirect, special, incidental,
+ *  or consequential damages of any character arising as a result of this License or
+ *  out of the use or inability to use the software (including but not limited to damages
+ *  for loss of goodwill, work stoppage, computer failure or malfunction, or any and
+ *  all other commercial damages or losses), even if such Contributor has been advised
+ *  of the possibility of such damages.
+ */
+
+#include "Assets/RadientMaterialAssetManager.hpp"
+
+#include "gtest/gtest.h"
+
+using namespace Diligent;
+
+namespace
+{
+
+TEST(RadientMaterialAssetManagerTest, CreateMaterial)
+{
+    // Creating a material should allocate a stable asset reference and build the
+    // internal GLTF material representation used by the renderer.
+    RadientMaterialAssetManagerSharedPtr pMaterialManager = RadientMaterialAssetManager::Create();
+    ASSERT_NE(pMaterialManager, nullptr);
+
+    RadientMaterialCreateInfo MaterialCI{};
+    MaterialCI.Name            = "Radient test material";
+    MaterialCI.BaseColorFactor = {0.25f, 0.5f, 0.75f, 0.8f};
+    MaterialCI.MetallicFactor  = 0.2f;
+    MaterialCI.RoughnessFactor = 0.7f;
+    MaterialCI.EmissiveFactor  = {1.f, 2.f, 3.f};
+    MaterialCI.AlphaCutoff     = 0.33f;
+    MaterialCI.DoubleSided     = True;
+
+    RefCntAutoPtr<IRadientMaterialAsset> pMaterial;
+    ASSERT_EQ(pMaterialManager->CreateMaterial(MaterialCI, &pMaterial), RADIENT_STATUS_OK);
+    ASSERT_NE(pMaterial, nullptr);
+    EXPECT_NE(pMaterial->GetReference().URI, nullptr);
+    EXPECT_NE(pMaterial->GetReference().Version, 0u);
+
+    const GLTF::Material* pGLTFMaterial = RadientMaterialAssetManager::GetMaterial(pMaterial);
+    ASSERT_NE(pGLTFMaterial, nullptr);
+
+    EXPECT_FLOAT_EQ(pGLTFMaterial->Attribs.BaseColorFactor.x, MaterialCI.BaseColorFactor.x);
+    EXPECT_FLOAT_EQ(pGLTFMaterial->Attribs.BaseColorFactor.y, MaterialCI.BaseColorFactor.y);
+    EXPECT_FLOAT_EQ(pGLTFMaterial->Attribs.BaseColorFactor.z, MaterialCI.BaseColorFactor.z);
+    EXPECT_FLOAT_EQ(pGLTFMaterial->Attribs.BaseColorFactor.w, MaterialCI.BaseColorFactor.w);
+    EXPECT_FLOAT_EQ(pGLTFMaterial->Attribs.MetallicFactor, MaterialCI.MetallicFactor);
+    EXPECT_FLOAT_EQ(pGLTFMaterial->Attribs.RoughnessFactor, MaterialCI.RoughnessFactor);
+    EXPECT_FLOAT_EQ(pGLTFMaterial->Attribs.EmissiveFactor.x, MaterialCI.EmissiveFactor.x);
+    EXPECT_FLOAT_EQ(pGLTFMaterial->Attribs.EmissiveFactor.y, MaterialCI.EmissiveFactor.y);
+    EXPECT_FLOAT_EQ(pGLTFMaterial->Attribs.EmissiveFactor.z, MaterialCI.EmissiveFactor.z);
+    EXPECT_FLOAT_EQ(pGLTFMaterial->Attribs.AlphaCutoff, MaterialCI.AlphaCutoff);
+    EXPECT_TRUE(pGLTFMaterial->DoubleSided);
+
+    EXPECT_EQ(pGLTFMaterial->GetTextureId(GLTF::DefaultBaseColorTextureAttribId), -1);
+    EXPECT_EQ(pGLTFMaterial->GetTextureId(GLTF::DefaultNormalTextureAttribId), -1);
+    EXPECT_EQ(pGLTFMaterial->GetTextureId(GLTF::DefaultMetallicRoughnessTextureAttribId), -1);
+}
+
+} // namespace
